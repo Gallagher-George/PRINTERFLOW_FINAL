bc45d9f6a34bb532cc2c010c5bd1f482f7f4662b
Gallagher <galgeow@dunwoody.edu>
2022-10-27 11:04:18 -0500

    Commits from October 25
    PHP actions.php for login and email verification code

---
+++ config/newfunctions.php
@@ -0,0 +1,335 @@
+<?php
+require_once 'config/db.php';
+$db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die("database is not connected");
+
+
+
+//function for showing pages
+function showPage($page,$data=""){
+include("pages/$page.php");
+}
+
+
+
+
+
+//for getting ids of chat users
+function getActiveChatUserIds(){
+    global $db;
+    $current_user_id = $_SESSION['userdata']['id'];
+    $query = "SELECT from_user_id,to_user_id FROM messages WHERE to_user_id=$current_user_id || from_user_id=$current_user_id ORDER BY id DESC";
+    $run = mysqli_query($db,$query);
+    $data =  mysqli_fetch_all($run,true);
+    $ids=array();
+    foreach($data as $ch){
+    if($ch['from_user_id']!=$current_user_id && !in_array($ch['from_user_id'],$ids)){
+       $ids[]=$ch['from_user_id'];
+    }
+
+
+
+   if($ch['to_user_id']!=$current_user_id && !in_array($ch['to_user_id'],$ids)){
+        $ids[]=$ch['to_user_id'];
+     }
+
+
+
+   }
+
+
+
+   return $ids;
+}
+
+
+
+function getMessages($user_id){
+    global $db;
+    $current_user_id = $_SESSION['userdata']['id'];
+    $query = "SELECT * FROM messages WHERE (to_user_id=$current_user_id && from_user_id=$user_id) || (from_user_id=$current_user_id && to_user_id=$user_id) ORDER BY id DESC";
+    $run = mysqli_query($db,$query);
+    return  mysqli_fetch_all($run,true);
+}
+
+
+
+function sendMessage($user_id,$msg){
+    global $db;
+    $current_user_id = $_SESSION['userdata']['id'];
+    $query = "INSERT INTO messages (from_user_id,to_user_id,msg) VALUES($current_user_id,$user_id,'$msg')";
+    return mysqli_query($db,$query);
+}
+
+
+
+function newMsgCount(){
+global $db;
+$current_user_id = $_SESSION['userdata']['id'];
+$query="SELECT COUNT(*) as row FROM messages WHERE to_user_id=$current_user_id && read_status=0";
+$run=mysqli_query($db,$query);
+return mysqli_fetch_assoc($run)['row'];
+}
+
+
+
+function updateMessageReadStatus($user_id){
+    $cu_user_id = $_SESSION['userdata']['id'];
+    global $db;
+    $query="UPDATE messages SET read_status=1 WHERE to_user_id=$cu_user_id && from_user_id=$user_id";
+    return mysqli_query($db,$query);
+}
+
+
+
+function gettime($date){
+    return date('H:i - (F jS, Y )', strtotime($date));
+}
+
+
+
+function getAllMessages(){
+    $active_chat_ids = getActiveChatUserIds();
+    $conversation=array();
+    foreach($active_chat_ids as $index=>$id){
+        $conversation[$index]['user_id'] = $id;
+        $conversation[$index]['messages'] = getMessages($id);
+    }
+    return $conversation;
+}
+
+
+
+//function for follow the user
+function followUser($user_id){
+    global $db;
+    $cu = getUser($_SESSION['userdata']['id']);
+    $current_user=$_SESSION['userdata']['id'];
+    $query="INSERT INTO follow_list(follower_id,user_id) VALUES($current_user,$user_id)";
+  
+    createNotification($cu['id'],$user_id,"started following you !");
+    return mysqli_query($db,$query);
+    
+}
+
+
+
+
+
+//function for blocking the user
+function blockUser($blocked_user_id){
+    global $db;
+    $cu = getUser($_SESSION['userdata']['id']);
+    $current_user=$_SESSION['userdata']['id'];
+    $query="INSERT INTO block_list(user_id,blocked_user_id) VALUES($current_user,$blocked_user_id)";
+  
+    createNotification($cu['id'],$blocked_user_id,"blocked you");
+    $query2="DELETE FROM follow_list WHERE follower_id=$current_user && user_id=$blocked_user_id";
+    mysqli_query($db,$query2);
+    $query3="DELETE FROM follow_list WHERE follower_id=$blocked_user_id && user_id=$current_user";
+    mysqli_query($db,$query3);
+
+
+
+  
+    return mysqli_query($db,$query);
+    
+}
+
+
+
+//for unblocking the user
+function unblockUser($user_id){
+    global $db;
+    $current_user=$_SESSION['userdata']['id'];
+    $query="DELETE FROM block_list WHERE user_id=$current_user && blocked_user_id=$user_id";
+    createNotification($current_user,$user_id,"Unblocked you !");
+    return mysqli_query($db,$query);   
+}
+
+
+
+//function checkLikeStatus
+function checkLikeStatus($post_id){
+    global $db;
+    $current_user = $_SESSION['userdata']['id'];
+    $query="SELECT count(*) as row FROM likes WHERE user_id=$current_user && post_id=$post_id";
+    $run = mysqli_query($db,$query);
+    return mysqli_fetch_assoc($run)['row'];
+}
+
+
+
+//function for like the post
+function like($post_id){
+    global $db;
+    $current_user=$_SESSION['userdata']['id'];
+    $query="INSERT INTO likes(post_id,user_id) VALUES($post_id,$current_user)";
+   $poster_id = getPosterId($post_id);
+   
+   if($poster_id!=$current_user){
+    createNotification($current_user,$poster_id,"liked your post !",$post_id);
+   }
+   
+
+
+
+   return mysqli_query($db,$query);
+    
+}
+
+
+
+
+
+
+//function for creating comments
+function addComment($post_id,$comment){
+    global $db;
+$comment = mysqli_real_escape_string($db,$comment);
+
+
+
+   $current_user=$_SESSION['userdata']['id'];
+    $query="INSERT INTO comments(user_id,post_id,comment) VALUES($current_user,$post_id,'$comment')";
+    $poster_id = getPosterId($post_id);
+
+
+
+   if($poster_id!=$current_user){
+        createNotification($current_user,$poster_id,"commented on your post",$post_id);
+    }
+   
+
+
+
+   return mysqli_query($db,$query);
+    
+}
+
+
+
+
+//function for creating comments
+function createNotification($from_user_id,$to_user_id,$msg,$post_id=0){
+    global $db;
+    $query="INSERT INTO notifications(from_user_id,to_user_id,message,post_id) VALUES($from_user_id,$to_user_id,'$msg',$post_id)";
+    mysqli_query($db,$query);    
+}
+
+
+
+
+
+//function for getting likes count
+function getComments($post_id){
+    global $db;
+    $query="SELECT * FROM comments WHERE post_id=$post_id ORDER BY id DESC";
+    $run = mysqli_query($db,$query);
+    return mysqli_fetch_all($run,true);
+}
+
+
+
+//get notifications
+
+
+
+function getNotifications(){
+  $cu_user_id = $_SESSION['userdata']['id'];
+
+
+
+   global $db;
+    $query="SELECT * FROM notifications WHERE to_user_id=$cu_user_id ORDER BY id DESC";
+    $run = mysqli_query($db,$query);
+    return mysqli_fetch_all($run,true);
+}
+
+
+
+
+
+function getUnreadNotificationsCount(){
+    $cu_user_id = $_SESSION['userdata']['id'];
+  
+      global $db;
+      $query="SELECT count(*) as row FROM notifications WHERE to_user_id=$cu_user_id && read_status=0 ORDER BY id DESC";
+      $run = mysqli_query($db,$query);
+      return mysqli_fetch_assoc($run)['row'];
+  }
+
+
+
+ function show_time($time){
+    return '<time style="font-size:small" class="timeago text-muted text-small" datetime="'.$time.'"></time>';
+  }
+
+
+
+ function setNotificationStatusAsRead(){
+       $cu_user_id = $_SESSION['userdata']['id'];
+      global $db;
+      $query="UPDATE notifications SET read_status=1 WHERE to_user_id=$cu_user_id";
+      return mysqli_query($db,$query);
+  }
+
+
+
+
+
+//function for getting likes count
+function getLikes($post_id){
+    global $db;
+    $query="SELECT * FROM likes WHERE post_id=$post_id";
+    $run = mysqli_query($db,$query);
+    return mysqli_fetch_all($run,true);
+}
+
+
+
+//function for unlike the post
+function unlike($post_id){
+    global $db;
+    $current_user=$_SESSION['userdata']['id'];
+    $query="DELETE FROM likes WHERE user_id=$current_user && post_id=$post_id";
+    
+    $poster_id = getPosterId($post_id);
+    if($poster_id!=$current_user){
+        createNotification($current_user,$poster_id,"unliked your post !",$post_id);
+    }
+  
+    return mysqli_query($db,$query);
+}
+
+
+
+
+function unfollowUser($user_id){
+    global $db;
+    $current_user=$_SESSION['userdata']['id'];
+    $query="DELETE FROM follow_list WHERE follower_id=$current_user && user_id=$user_id";
+
+
+
+   createNotification($current_user,$user_id,"Unfollowed you !");
+    return mysqli_query($db,$query);
+
+    
+}
+
+
+
+
+//function for show errors
+function showError($field){
+if(isset($_SESSION['error'])){
+    $error =$_SESSION['error'];
+    if(isset($error['field']) && $field==$error['field']){
+       ?>
+<div class="alert alert-danger my-2" role="alert">
+  <?=$error['msg']?>
+</div>
+       <?php
+    }
+}
+}
