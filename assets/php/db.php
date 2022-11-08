212852f021389b94d66f439a67d0f30764e7ce28
George <george.gallagher.w@gmail.com>
2022-11-08 10:21:15 -0600

    Updates as of November 8
    Web App should be up and running except for media player

--- assets/php/db.php
+++ assets/php/db.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 //mysqli dab
-const DB_NAME='finalprinterflowofficial';
+const DB_NAME='dunwoodyfinaldb';
 const DB_HOST='localhost';
 const DB_USER='root';
 const DB_PASS='';
-
-
-
-?>
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<?php
+<?php 
 
-require_once'config/functions.php';
+require_once'assets/php/functions.php';
 if(isset($_GET['newfp']))
 {
-    unset($_SESSION['auth_temp']);
-    unset($_SESSION['forgot_email']);
-    unset($_SESSION['forgot_code']);
+
+	unset($_SESSION['auth_temp']);
+	unset($_SESSION['forgot_email']);
+	unset($_SESSION['forgot_code']);
 }
-if(isset($_SESSION['Auth']))
-{
+
+if(isset($_SESSION['Auth'])){
     $user = getUser($_SESSION['userdata']['id']);
     $posts = filterPosts();
     $follow_suggestions = filterFollowSuggestion();
@@ -16,90 +17,73 @@ if(isset($_SESSION['Auth']))
 
 $pagecount = count($_GET);
 
+
 //manage pages
-if(isset($_SESSION['Auth']) && $user['ac_status']==1 && !$pagecount)
-{
+if(isset($_SESSION['Auth']) && $user['ac_status']==1 && !$pagecount){
     showPage('header',['page_title'=>'Home']);
     showPage('navbar');
-    showPage('edit_profile');
-}
-elseif(isset($_SESSION['Auth']) && $user['ac_status']==0 && !$pagecount)
-{
-    showPage('header',['page_title'=>'Verify You Email']);
+    showPage('wall');
+}elseif(isset($_SESSION['Auth']) && $user['ac_status']==0 && !$pagecount){
+
+    showPage('header',['page_title'=>'Verify Your Email']);
     showPage('verify_email');
-}
-elseif(isset($_SESSION['Auth']) && $user['ac_status']==2 && !$pagecount)
-{
+}elseif(isset($_SESSION['Auth']) && $user['ac_status']==2 && !$pagecount){
     showPage('header',['page_title'=>'Blocked']);
-    showPage('Blocked');
-}
-elseif(isset($_SESSION['Auth']) && isset($_GET['editprofile']) && $user['ac_status']==1)
-{
-    showPage('header',['page_title'=>'Profile']);
+    showPage('blocked');
+	
+}elseif(isset($_SESSION['Auth']) && isset($_GET['editprofile']) && $user['ac_status']==1){
+    showPage('header',['page_title'=>'Edit Profile']);
     showPage('navbar');
     showPage('edit_profile');
-}
-elseif(isset($_SESSION['Auth']) && isset($_GET['u']) && $user['ac_status']==1)
-{
-    $profile=getUserByUsername($_GET['u']);
-    if(!$profile)
-    {
+}elseif(isset($_SESSION['Auth']) && isset($_GET['u']) && $user['ac_status']==1){
+    $profile = getUserByUsername($_GET['u']);
+    if(!$profile){
         showPage('header',['page_title'=>'User Not Found']);
         showPage('navbar');
         showPage('user_not_found');
-    }
-    else
-    {
-        $profile_post=getPostById($profile['id']);
-        $profile['followers']=getFollowers($profile['id']);
-        $profile['following']=getFollowing($profile['id']);
+
+    }else{
+     $profile_post = getPostById($profile['id']);  
+     $profile['followers']=getFollowers($profile['id']);
+     $profile['following']=getFollowing($profile['id']);
         showPage('header',['page_title'=>$profile['first_name'].' '.$profile['last_name']]);
         showPage('navbar');
         showPage('profile');
     }
-}
-
-elseif(isset($_GET['signup']))
-{
+ 
+  
+}elseif(isset($_GET['signup'])){
     showPage('header',['page_title'=>'SignUp']);
-    showPage('navbar');
     showPage('signup');
-}
-elseif(isset($_GET['login']))
-{
+}elseif(isset($_GET['login'])){
+   
     showPage('header',['page_title'=>'Login']);
     showPage('login');
-}
-elseif(isset($_GET['forgotpassword']))
-{
-    showPage('header',['page_title'=>'forgotpassword']);
+}elseif(isset($_GET['forgotpassword'])){
+    
+    showPage('header',['page_title'=>'Forgot Password']);
     showPage('forgot_password');
-}
-else
-{
-    if(isset($_SESSION['Auth']) && $user['ac_status']==1)
-    {
+}else{
+    if(isset($_SESSION['Auth']) && $user['ac_status']==1){
         showPage('header',['page_title'=>'Home']);
         showPage('navbar');
         showPage('wall');
-    }
-    elseif(isset($_SESSION['Auth']) && $user['ac_status']==0)
-    {
+    }elseif(isset($_SESSION['Auth']) && $user['ac_status']==0){
+
         showPage('header',['page_title'=>'Verify Your Email']);
-        showPage('verfiy_email');
-    }
-    elseif(isset($_SESSION['Auth']) && $user['ac_status']==2)
-    {
+        showPage('verify_email');
+    }elseif(isset($_SESSION['Auth']) && $user['ac_status']==2){
         showPage('header',['page_title'=>'Blocked']);
         showPage('blocked');
-    }
-    else{
+    }else{
         showPage('header',['page_title'=>'Login']);
         showPage('login');
     }
+  
 }
+
 
 showPage('footer');
 unset($_SESSION['error']);
-unset($_SESSION['formdata'])
-?>
+unset($_SESSION['formdata']);
+
